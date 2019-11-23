004c159b684f3ed8736d3b3803e49da453935750
0b10 <0b10@pm.me>
2019-11-23 02:34:23 +0000

    Make switch
       ...to handle backup and restore, help etc.

--- dnf-userinstalled.plugin.zsh
+++ dnf-userinstalled.plugin.zsh
@@ -2,16 +2,36 @@
 
 DNF_USERINSTALLED_TARGET="/tmp/foo/target.txt"
 
-function userinstalled () {
-    [[ -z $DNF_USERINSTALLED_TARGET ]] && \
-    echo "you must export DNF_USERINSTALLED_TARGET from your zshrc" && return 1;
-    
+function _userinstalled_backup () {
     if [ ! -d `dirname $DNF_USERINSTALLED_TARGET` ]; then
         mkdir -p `dirname $DNF_USERINSTALLED_TARGET`;
     fi
     
     sudo dnf history userinstalled | \
-    perl -pe 's/(-[0-9]+[-.:\w]+$)|(Packages installed by user\n)|(^ *$)//gm' > \ # trim version numbers and arch
+    perl -pe 's/(-[0-9]+[-.:\w]+$)|(Packages installed by user\n)|(^ *$)//gm' > \
     $DNF_USERINSTALLED_TARGET && \
     echo "userinstalled: package names backed up";
 }
+
+function _userinstalled_restore () {
+    echo restoring
+}
+
+function userinstalled () {
+    [[ -z $DNF_USERINSTALLED_TARGET ]] && \
+    echo "you must export DNF_USERINSTALLED_TARGET from your zshrc" && return 1;
+    
+    case "$1" in
+        "backup"|"-b"|"--backup")
+            _userinstalled_backup
+        ;;
+        "restore"|"-r"|"--restore")
+            _userinstalled_restore
+        ;;
+        *)
+            echo "userinstalled: unknown option -- '${1}'"
+        ;;
+    esac
+}
+
+userinstalled $@
