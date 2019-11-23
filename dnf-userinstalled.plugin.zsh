929d015176f40dbd7242ff81b7668bf4819fb5d8
0b10 <0b10@pm.me>
2019-11-23 02:25:24 +0000

    Trim version numbers and arch from output
       ...so that all that remains is an easy list to install

--- dnf-userinstalled.plugin.zsh
+++ dnf-userinstalled.plugin.zsh
@@ -3,14 +3,15 @@
 DNF_USERINSTALLED_TARGET="/tmp/foo/target.txt"
 
 function userinstalled () {
-    [[ -z $DNF_USERINSTALLED_TARGET ]] && echo "you must export DNF_USERINSTALLED_TARGET from your zshrc" && return 1;
+    [[ -z $DNF_USERINSTALLED_TARGET ]] && \
+    echo "you must export DNF_USERINSTALLED_TARGET from your zshrc" && return 1;
     
     if [ ! -d `dirname $DNF_USERINSTALLED_TARGET` ]; then
         mkdir -p `dirname $DNF_USERINSTALLED_TARGET`;
     fi
     
-    sudo dnf history userinstalled > $DNF_USERINSTALLED_TARGET && \
+    sudo dnf history userinstalled | \
+    perl -pe 's/(-[0-9]+[-.:\w]+$)|(Packages installed by user\n)|(^ *$)//gm' > \ # trim version numbers and arch
+    $DNF_USERINSTALLED_TARGET && \
     echo "userinstalled: package names backed up";
 }
-
-userinstalled
