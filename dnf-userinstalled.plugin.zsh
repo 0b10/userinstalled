147a39a4a6522e2a0ef0a370ddb4fdaf45485a64
0b10 <0b10@pm.me>
2019-11-23 16:24:54 +0000

    Finish restore function, and use repoquery, not history
      make it load backed up packages from the file, and installed them
      Also, use repoquery, because the output can be formatted to just
      include package names without needing to search and replace.

--- dnf-userinstalled.plugin.zsh
+++ dnf-userinstalled.plugin.zsh
@@ -2,27 +2,27 @@
 
 zmodload zsh/mapfile;
 
-DNF_USERINSTALLED_TARGET="/tmp/foo/target.txt"
+DNF_USERINSTALLED_FILE="/tmp/foo/target.txt"
 
 function _userinstalled_backup () {
-    if [ ! -d `dirname $DNF_USERINSTALLED_TARGET` ]; then
-        mkdir -p `dirname $DNF_USERINSTALLED_TARGET`;
+    if [ ! -d `dirname $DNF_USERINSTALLED_FILE` ]; then
+        mkdir -p `dirname $DNF_USERINSTALLED_FILE`;
     fi
     
-    sudo dnf history userinstalled | \
-    perl -pe 's/(-[0-9]+[-.:\w]+$)|(Packages installed by user\n)|(^ *$)//gm' > \
-    $DNF_USERINSTALLED_TARGET && \
+    # exclude zsh, because it needs to be installed prior to running this script
+    # TODO: add ignore packages
+    dnf repoquery --userinstalled --queryformat %{name} | grep -v "zsh" > $DNF_USERINSTALLED_FILE
     echo "userinstalled: package names backed up";
 }
 
 function _userinstalled_restore () {
-    local packages=$mapfile[${DNF_USERINSTALLED_TARGET}];
-    echo $packages
+    local packages=("${(f@)mapfile[${DNF_USERINSTALLED_FILE}]}")
+    sudo dnf install ${packages[@]} # don't make string, causes issues
 }
 
 function userinstalled () {
-    [[ -z $DNF_USERINSTALLED_TARGET ]] && \
-    echo "you must export DNF_USERINSTALLED_TARGET from your zshrc" && return 1;
+    [[ -z $DNF_USERINSTALLED_FILE ]] && \
+    echo "you must export DNF_USERINSTALLED_FILE from your zshrc" && return 1;
     
     case "$1" in
         "backup"|"-b"|"--backup")
