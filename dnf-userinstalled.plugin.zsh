6d128d8549522ef072f340297faa35fb6e326bd7
0b10 <0b10@pm.me>
2019-11-23 01:53:45 +0000

    Implement logic to make the target directory
       ...so that the target file can be created

--- dnf-userinstalled.plugin.zsh
+++ dnf-userinstalled.plugin.zsh
@@ -1,7 +1,14 @@
 #!/usr/bin/zsh
 
+DNF_USERINSTALLED_TARGET="/tmp/foo/target.txt"
+
 function userinstalled () {
-    [[ -z $DNF_USERINSTALLED_TARGET ]] && echo "you must export DNF_USERINSTALLED_TARGET";
+    [[ -z $DNF_USERINSTALLED_TARGET ]] && echo "you must export DNF_USERINSTALLED_TARGET from your zshrc" && return 1;
+    
+    if [ ! -d `dirname $DNF_USERINSTALLED_TARGET` ]; then
+        mkdir -p `dirname $DNF_USERINSTALLED_TARGET`;
+    fi
+    
 }
 
 userinstalled
